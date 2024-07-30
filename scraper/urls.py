82ef207063644f712e4a9f29691f120550c0acc6
lechendemstephen <51188322+lechendemstephen@users.noreply.github.com>
2024-07-29 21:56:02 -0400

    finalizing the scraping app

--- scraper/urls.py
+++ scraper/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.scrape, name='home')
+    path('', views.scrape, name='home'),
+
+    path('delete/', views.delete, name='delete')
 ]
--- scraper/views.py
+++ scraper/views.py
@@ -1,23 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests 
 from bs4 import BeautifulSoup
 from .models import Link
+from .forms import LinkForm
 # Create your views here.
 
-def scrape(request): 
-    page = requests.get('https://www.google.com/')
-    soup = BeautifulSoup(page.text, 'html.parser')
+def scrape(request):
+    if request.method == "POST": 
 
-    for link in soup.find_all('a'): 
-        link_address = link.get('href')
-        link_text = link.string
-        Link.objects.create(
-            address = link_address,
-            name = link_text
-        )
-    data = Link.objects.all()
+            website = request.POST.get('site', '')
 
+            page = requests.get(website)
+            soup = BeautifulSoup(page.text, 'html.parser')
+
+            for link in soup.find_all('a'): 
+                link_address = link.get('href')
+                link_text = link.string
+                Link.objects.create(
+                    address = link_address,
+                    name = link_text
+                )
+                return redirect('home')
 
     return render(request, 'scraper/result.html', {
-        'links': data,
+        'links': Link.objects.all(),
     })
+
+
+def delete(request): 
+     data = Link.objects.all()
+     data.delete()
+     return redirect('home')
+     
+
+
+
+     
+     
+     
